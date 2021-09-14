1a92014d1171f1fefdcc055ece8699651942eaac
김범수 <qhrud0527@naver.com>
2021-09-14 17:38:25 +0900

    FE / 회원가입 / 진행중

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,36 +6,68 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    duplicateCheck: {
-      id: false,
-      nickname: false
+    // 회원가입시 ID, Nickname, Password State
+    signupCheck: {
+      id: {
+        condition: {
+          flag : false,
+          msg : "ID는 4자 이상이여야 합니다."
+        },
+        duplicate : {
+          flag : false,
+          msg : "중복된 ID가 존재합니다."
+        },
+      },
+      nickname: {
+        condition: {
+          flag : false,
+          msg : "닉네임은 3자 이상이여야 합니다."
+        },
+        duplicate : {
+          flag : false,
+          msg : "중복된 닉네임이 존재합니다."
+        },
+      },
+      password: {
+        condition: {
+          flag : false,
+          msg : "Password는 6자 이상이여야 합니다."
+        }
+      }
+    }
+  },
+  getters: {
+    getSignupCheck(state) {
+      return state.signupCheck;
     }
   },
   mutations: {
-    // ID 중복 유무
-    DUPLICATE_CHECK_ID(state, result) {
-      state.duplicateCheck.id = result;
-    },
-    // Nickname 중복 유무
-    DUPLICATE_CHECK_NAME(state, result) {
-      state.duplicateCheck.nickname = result;
+    // 회원가입 검사
+    SIGNUP_CHECK(state, result) {
+      if (result.type != 'password') {
+        state.signupCheck[result.type].duplicate.flag = result.duplicate;
+      }
+      state.signupCheck[result.type].condition.flag = result.condition;
     }
   },
   actions: {
-    // 중복 체크
-    duplicateCheck({ commit }, data) {
+    // 회원가입시 ID, Nickname, Password 검사
+    signupCheck({ commit }, req) {
+      var result = {
+        type: req.val,
+        condition: req.condition
+      };
+      if (req.val !== 'password') {
         http
-          .get('/auth/user/valDuplicated', data)
+          .get('/auth/user/valDuplicated', req)
           .then(({ res }) => {
-            if (data.val === 'id') { // ID 중복 유무
-              commit('DUPLICATE_CHECK_ID', res.result); 
-            } else { // // ID 중복 유무
-              commit('DUPLICATE_CHECK_NAME', res.result);
-            }
+              result["duplicate"] = res.result;
           })
           .catch(() => {
             console.log("500에러");
           })
       }
+      commit("SIGNUP_CHECK", result);
     }
+  }
 });
--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -7,22 +7,49 @@
                         <login-card header-color="green">
                             <h4 slot="title" class="card-title">회원 가입</h4>
                             <p slot="description" class="description">Or Be Classical</p>
-                            <md-field class="md-form-group" slot="inputs">
+
+                            <md-field slot="inputs" :class="{'md-invalid': !getSignupCheck.id.condition.flag && !getSignupCheck.id.duplicate.flag}">
                                 <md-icon>face</md-icon>
                                 <label>ID</label>
                                 <md-input v-model="id"></md-input>
+                                <span class="md-error">안돼</span>
+                                <span class="md-helper-text">{{getSignupCheck.id.condition.msg}}</span>
                             </md-field>
-                            <md-field class="md-form-group" slot="inputs">
+
+                            <md-field slot="inputs" :class="{'md-invalid': !getSignupCheck.nickname.condition.flag && !getSignupCheck.nickname.duplicate.flag}">
                                 <md-icon>supervisor_account</md-icon>
                                 <label>NickName</label>
-                                <md-input v-model="nickname" ></md-input>
+                                <md-input v-model="nickname"></md-input>
+                                <span class="md-helper-text">{{getSignupCheck.nickname.condition.msg}}</span>
+                                <span class="md-error">안돼</span>
                             </md-field>
-                            <md-field class="md-form-group" slot="inputs">
+
+                            <md-field :class="messageClass" slot="inputs">
                                 <md-icon>lock</md-icon>
                                 <label>Password</label>
-                                <md-input v-model="password"></md-input>
+                                <md-input v-model="password" type="password" required></md-input>
+                                <span class="md-error">안돼</span>
+                                <span class="md-helper-text">{{getSignupCheck.password.condition.msg}}</span>
                             </md-field>
-                            <md-button slot="footer" class="md-simple md-success md-lg">
+
+                            <md-field slot="inputs" :class="[!getSignupCheck.password.condition.flag ? 'md-error' : 'md-valid']">
+                                <label>Success Input</label>
+                                <md-input v-model="success" type="text"></md-input>
+                                <div v-if="!getSignupCheck.password.condition.flag">
+                                    <md-icon>clear</md-icon>
+                                </div>
+                                <div v-else>
+                                    <md-icon>done</md-icon>
+                                </div>
+                            </md-field>
+
+                            <md-field class="md-error">
+                                <label>Error Input</label>
+                                <md-input v-model="error" type="text"></md-input>
+                                <md-icon>{{icon.password}}</md-icon>
+                            </md-field>
+                            
+                            <md-button slot="footer" class="md-simple md-success md-lg" @click="duplicateCheck('password')">
                                 Get Started
                             </md-button>
                         </login-card>
@@ -44,9 +71,14 @@ export default {
     bodyClass: "login-page",
     data() {
         return {
-        id: null,
-        nickname: null,
-        password: null
+            id: "",
+            nickname: "",
+            password: "",
+            icon : {
+                id : "",
+                nickname : "",
+                password : ""
+            }
         };
     },
     props: {
@@ -61,19 +93,34 @@ export default {
                 backgroundImage: `url(${this.header})`
             };
         },
-        ...mapGetters(['duplicateCheck'])
+        messageClass () {
+            return {
+                'md-invalid' : !this.getSignupCheck.password.condition.flag
+            }
+        },
+        ...mapGetters(['getSignupCheck'])
     },
     methods : {
+        conditionCheck : function(type){
+            if(type === 'id'){ // ID 조건
+                return this.id.length > 4 ? true : false
+            }else if(type === 'nickname'){ // 닉네임 조건
+                return this.nickname.length > 3 ? true : false
+            }else{ // Password 조건
+                return this.password.length > 5 ? true : false
+            }
+        },
         duplicateCheck : function(type){
             var data = {
-                val : type
+                condition : this.conditionCheck(type),
+                val : type,
             };
             if(type === 'id'){
                 data["content"] = this.id;
-            }else{
+            }else {
                 data["content"] = this.nickname
             }
-            this.$store.dispatch('duplicateCheck', data);
+            this.$store.dispatch('signupCheck', data);
         }
     }
 };
