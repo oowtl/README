cf0574c0cf5a04314596c90bc421be8db589a4e8
박준홍 <oowtl7425@gmail.com>
2021-09-14 18:55:22 +0900

    BDT / 크롤링하기 / 0914 개발 중

--- study/recommend/Data-crawling/src/study_code/hi_selenium.py
+++ study/recommend/Data-crawling/src/study_code/hi_selenium.py
@@ -118,8 +118,18 @@ print('------pattern')
 # 대기
 driver.implicitly_wait(time_to_wait=5)
 
+# 카테고리 부분이 몇개나 있는지
 book_menu_all = driver.find_elements_by_xpath('//*[@id="main_snb"]/div[1]/ul')
-print(len(book_menu_all))
+
+# 국내 도서 카테고리 선택으로 돌아오기
+
+# 1번 : //*[@id="gnb_category"]/a
+# 2번 : //*[@id="gnb_menu01"]/div[1]
+# 3번 : 클릭
+
+
+
+
 
 print('------mouse')
 # 각 장르 별로 하나 씩 해야 한다.
@@ -146,16 +156,81 @@ for i in range(1, len(book_menu_all)):
             actions_small.click()
             actions_small.perform()
 
+            # 소분류로 입장을 하고 나서 대기
             driver.implicitly_wait(time_to_wait=5)
+
+            # 페이지를 파악하자
+            # 이 페이지는 페이지네이션이 2개가 들어갑니다.
+            # 현재 xpath 는 상단에 들어가있는 것입니다.
+            book_item_pagination = driver.find_elements_by_xpath('//*[@id="eventPaging"]/div/ul/li')
+
+            print(len(book_item_pagination))
+
+            for l in range(1, (len(book_item_pagination)//2)+1):
+
+                # 페이지네이션으로 들어 간다.
+                action_to_pagination = ActionChains(driver)
+                action_to_pagination.move_to_element(driver.find_element_by_xpath('//*[@id="eventPaging"]/div/ul/li[{}]'.format(l)))
+                action_to_pagination.click()
+                action_to_pagination.perform()
+
+                # 대기시간
+                driver.implicitly_wait(time_to_wait=5)
+
+                # 10이면 '>' 선택하고 다시 페이지네이션 돌려야 합니다.
+                if l == 10:
+
+                    
+                    break
+
+
+
+            driver.quit()
+            exit()
+
+        
+
+
+            # # 한 페이지에 나오는 책의 갯수
+            # book_item_all = driver.find_elements_by_xpath('//*[@id="prd_list_type1"]/li')
             
-            # 사이트 나오기
-            driver.back()
+            # # print(len(book_item_all)) # 40 (20개) 1, 3, 5, 7, ...
+
+            # # 20개 리스트를 하나씩 클릭한다.
+            # for m in range(1, (len(book_item_all)//2)+1):
+            #     action_to_item = ActionChains(driver)
+            #     action_to_item.move_to_element(driver.find_element_by_xpath('//*[@id="prd_list_type1"]/li[{}]/div/div[1]/div[2]/div[1]/a'.format((m*2)-1)))
+            #     action_to_item.click()
+            #     action_to_item.perform()
+
+            #     # 페이지 로드 대기
+            #     driver.implicitly_wait(time_to_wait=10)
+
+            #     # 현 상황 : book item 페이지에 들어가있는 상황
+                
+
+            #     driver.back()
+            #     driver.implicitly_wait(time_to_wait=5)
+
+        
+            # # 종료
+            # driver.quit()
+            # break
 
 
-            # 다시 선택하기?
-            actions_small_back = ActionChains(driver)
-            actions_small_back.move_to_element(driver.find_element_by_xpath('//*[@id="main_snb"]/div[1]/ul[{}]/li[{}]/a'.format(i, j)))
-            actions_small_back.perform()
+            # # 국내도서 카테고리로 돌아가기
+            # action_to_category = ActionChains(driver)
+            # action_to_category.move_to_element(driver.find_element_by_xpath('//*[@id="gnb_category"]/a'))
+            # action_to_category.move_to_element(driver.find_element_by_xpath('//*[@id="gnb_menu01"]/div[1]'))
+            # action_to_category.click()
+            # action_to_category.perform()
+
+
+
+            # # 다시 마우스를 호버 해줘야 한다.
+            # actions_small_back = ActionChains(driver)
+            # actions_small_back.move_to_element(driver.find_element_by_xpath('//*[@id="main_snb"]/div[1]/ul[{}]/li[{}]/a'.format(i, j)))
+            # actions_small_back.perform()
 
             
         # 각 호버되는 것들의 패턴
@@ -170,7 +245,22 @@ for i in range(1, len(book_menu_all)):
         # 인문 - 인문학 일반 : //*[@id="main_snb"]/div[1]/ul[2]/li[1]/ul/li[1]/a
         # 인문 - 심리학      : //*[@id="main_snb"]/div[1]/ul[2]/li[1]/ul/li[2]/a
 
-        
+
+        # 책 목록 패턴 (한국소설)
+        # 1번  : //*[@id="prd_list_type1"]/li[1]/div/div[1]/div[2]/div[1]/a
+        # 2번  : //*[@id="prd_list_type1"]/li[3]/div/div[1]/div[2]/div[1]/a
+        # 3번  : //*[@id="prd_list_type1"]/li[5]/div/div[1]/div[2]/div[1]/a
+        # 4번  : //*[@id="prd_list_type1"]/li[7]/div/div[1]/div[2]/div[1]/a
+        # 20번 : //*[@id="prd_list_type1"]/li[39]/div/div[1]/div[2]/div[1]/a
+
+
+        # 페이지네이션 패턴 (한국소설)
+        # 1번  : //*[@id="eventPaging"]/div/ul/li[1]/a (//*[@id="eventPaging"]/div/ul/li[1]/strong/a)
+        # 2번  : //*[@id="eventPaging"]/div/ul/li[2]/a
+        # 3번  : //*[@id="eventPaging"]/div/ul/li[3]/a
+        # 10번 : //*[@id="eventPaging"]/div/ul/li[10]/a
+        # >    : //*[@id="eventPaging"]/div/a[2]
+
 
 
 # 필요하다... 이건 웹에서 보여야 하는 것 같다. html 으로는 아닌듯
@@ -178,19 +268,6 @@ for i in range(1, len(book_menu_all)):
 # actions.move_to_element(domestic_menu)
 # actions.perform()
 # print("move mouse")
-
-
-# hover 된 메뉴 파악
-# //*[@id="main_snb"]/div[1]/ul[1]/li[1]/ul/li[1]
-# t2 = driver.find_elements_by_xpath('//*[@id="main_snb"]/div[1]/ul[1]/li[1]/ul/li[1]/a')
-
-# t2 = driver.find_elements_by_xpath('//*[@id="main_snb"]/div[1]/ul[1]/li[1]/ul/li')
-# for i in t2:
-#     # print("{} 이동".format(i.text))
-#     # i.click()
-#     # break
-
-#     print("{} : check".format(i.text))
 
 
 # driver.quit()
