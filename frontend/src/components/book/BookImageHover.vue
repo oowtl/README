a8cb9d27a1ad00df0df1f63ae88a827d7afc1b97
김범수 <qhrud0527@naver.com>
2021-09-28 17:02:05 +0900

    FE / 책 성향 페이지 조정 / 완료

---
+++ frontend/src/components/book/BookImageHover.vue
@@ -0,0 +1,204 @@
+<template>
+    <div>
+        <div>
+            <figure class="snip1273">
+                <img :src="book.img" width="300px" height="450px"/>
+                <figcaption>
+                    <h3>{{book.title}}</h3>
+                    <p>{{book.image}}</p>
+                    
+                </figcaption>
+                <a href="#"></a>
+            </figure>
+        </div>
+        <div class="md-layout">
+            <div class="md-layout-item"/>
+            <div class="md-layout-item" @click="getBookInfo(2)">
+                <md-avatar class="md-avatar-icon md-accent" >
+                    <md-ripple>
+                        <md-icon>sentiment_very_dissatisfied</md-icon>
+                    </md-ripple>
+                </md-avatar>
+            </div>  
+            <div class="md-layout-item" @click="getBookInfo(1)">
+                <md-avatar class="md-avatar-icon" >
+                    <md-ripple>
+                        <md-icon>sentiment_neutral</md-icon>
+                    </md-ripple>
+                </md-avatar>
+            </div>  
+            <div class="md-layout-item" @click="getBookInfo(0)">
+                <md-avatar class="md-avatar-icon md-primary" >
+                    <md-ripple>
+                        <md-icon>sentiment_very_satisfied</md-icon>
+                    </md-ripple>
+                </md-avatar>
+            </div>  
+            <div class="md-layout-item"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+    created(){
+        this.book = this.getBookPropensity[0]
+    },
+    data() {
+        return {
+            responsive: false,
+            index : 0,
+            book : {}
+        };
+    },
+    methods: {
+        getBookInfo(flag){
+            if(this.index < 3){
+                console.log(flag);
+                this.index += 1;
+                this.book = this.getBookPropensity[this.index];
+                console.log(this.index);
+                console.log(this.book);
+            }else{
+                alert("끝");
+            }
+        }
+    },
+    computed : {
+        ...mapGetters(['getBookPropensity']),
+    },
+    watch : {
+        index : function(){
+            this.book = this.getBookPropensity[this.index];
+        }
+    }
+}
+</script>
+
+<style>
+    @import url(https://fonts.googleapis.com/css?family=Raleway:400,500,700);
+    .snip1273 {
+        font-family: 'Raleway', Arial, sans-serif;
+        position: relative;   
+        margin-left: 36%;
+        max-width: 300px;
+        width: 100%;
+        color: #ffffff;
+        text-align: left;
+        background-color: #000000;
+        font-size: 16px;
+    }
+    .snip1273 * {
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        -webkit-transition: all 0.4s ease-in;
+        transition: all 0.4s ease-in;
+    }
+    .snip1273 img {
+        position: relative;
+        max-width: 100%;
+        vertical-align: top;
+    }
+    .snip1273 figcaption {
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 1;
+        opacity: 0;
+        padding: 20px 30px;
+    }
+    .snip1273 figcaption:before,
+    .snip1273 figcaption:after {
+        width: 1px;
+        height: 0;
+    }
+    .snip1273 figcaption:before {
+        right: 0;
+        top: 0;
+    }
+    .snip1273 figcaption:after {
+        left: 0;
+        bottom: 0;
+    }
+    .snip1273 h3,
+    .snip1273 p {
+        line-height: 1.5em;
+    }
+    .snip1273 h3 {
+        margin: 0 0 5px;
+        font-weight: 700;
+        text-transform: uppercase;
+    }
+    .snip1273 p {
+        font-size: 0.8em;
+        font-weight: 500;
+        margin: 0 0 15px;
+    }
+    .snip1273 a {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        z-index: 1;
+    }
+    .snip1273:before,
+    .snip1273:after,
+    .snip1273 figcaption:before,
+    .snip1273 figcaption:after {
+        position: absolute;
+        content: '';
+        background-color: #ffffff;
+        z-index: 1;
+        -webkit-transition: all 0.4s ease-in;
+        transition: all 0.4s ease-in;
+        opacity: 0.8;
+    }
+    .snip1273:before,
+    .snip1273:after {
+        height: 1px;
+        width: 0%;
+    }
+    .snip1273:before {
+        top: 0;
+        left: 0;
+    }
+    .snip1273:after {
+        bottom: 0;
+        right: 0;
+    }
+    .snip1273:hover img,
+    .snip1273.hover img {
+        opacity: 0.4;
+    }
+    .snip1273:hover figcaption,
+    .snip1273.hover figcaption {
+        opacity: 1;
+    }
+    .snip1273:hover figcaption:before,
+    .snip1273.hover figcaption:before,
+    .snip1273:hover figcaption:after,
+    .snip1273.hover figcaption:after {
+        height: 100%;
+    }
+    .snip1273:hover:before,
+    .snip1273.hover:before,
+    .snip1273:hover:after,
+    .snip1273.hover:after {
+        width: 100%;
+    }
+    .snip1273:hover:before,
+    .snip1273.hover:before,
+    .snip1273:hover:after,
+    .snip1273.hover:after,
+    .snip1273:hover figcaption:before,
+    .snip1273.hover figcaption:before,
+    .snip1273:hover figcaption:after,
+    .snip1273.hover figcaption:after {
+        opacity: 0.1;
+    }
+
+</style>
--- frontend/src/components/signup/BookPropensityTest.vue
+++ frontend/src/components/signup/BookPropensityTest.vue
@@ -1,38 +1,8 @@
 /<template>
     <div class="md-layout">
-        <div class="md-layout-item md-size-20 outer">
-            <div class="inner">
-                <md-button>
-                    <md-icon>arrow_back_ios</md-icon>    
-                </md-button>
-            </div>
-        </div>
-        <div
-        class="md-layout-item"
-        :class="{ 'text-center': responsive }">
-            <figure class="snip1382">
-            <img
-            :src="image"
-            alt="Rounded Image"
-            class="rounded"
-            :class="{ 'responsive-image': responsive }"
-            />
-            <figcaption>
-                <h2>Gustav Purpleson</h2>
-                <p>Weekends don't count unless you spend them doing something completely pointless.</p>
-                <div class="icons"><a href="#"><i class="ion-chatbubbles"></i></a>
-                <a href="#"> <i class="ion-person-add"></i></a>
-                <a href="#"> <i class="ion-heart"></i></a>
-                </div>
-            </figcaption>
-            </figure>
-            <img
-            :src="image"
-            alt="Rounded Image"
-            class="rounded"
-            :class="{ 'responsive-image': responsive }"
-            />
-            <div class="md-layout">
+        <div class="md-layout-item" :class="{ 'text-center': responsive }">
+            <book-image-hover/>
+            <!-- <div class="md-layout">
                 <div class="md-layout-item"/>
                 <div class="md-layout-item">
                     <md-avatar class="md-avatar-icon md-accent">
@@ -56,24 +26,18 @@
                     </md-avatar>
                 </div>  
                 <div class="md-layout-item"/>
-            </div>
-        </div>
-        <div class="md-layout-item md-size-20 outer">
-            <div class="inner">
-                <md-button>
-                    <md-icon>arrow_forward_ios</md-icon>    
-                </md-button>
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
 
 <script>
+import BookImageHover from '../book/BookImageHover.vue';
 export default {
+  components: { BookImageHover },
     data() {
         return {
-            image: require("@/assets/img/book.jpg"),
-            responsive: false
+            responsive: false,
         };
     },
     methods: {
@@ -103,137 +67,4 @@ export default {
     .inner {
         margin : auto;
     }
-@import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,500,900);
-@import url(https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css);
-figure.snip1382 {
-    font-family: 'Source Sans Pro', Arial, sans-serif;
-    position: relative;
-    overflow: hidden;
-    margin: 10px;
-    min-width: 230px;
-    max-width: 315px;
-    max-height: 220px;
-    width: 100%;
-    color: #ffffff;
-    text-align: center;
-    font-size: 16px;
-}
-figure.snip1382 * {
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-    -webkit-transition: all 0.35s ease;
-    transition: all 0.35s ease;
-}
-figure.snip1382 img {
-    max-width: 100%;
-}
-figure.snip1382:after,
-figure.snip1382:before,
-figure.snip1382 figcaption:after,
-figure.snip1382 figcaption:before {
-    background: #0a0a0a;
-    height: 25%;
-    position: absolute;
-    content: '';
-    opacity: 0;
-    -webkit-transition: all 0.35s steps(4);
-    transition: all 0.35s steps(4);
-    z-index: 1;
-    left: 50%;
-    right: 50%;
-}
-figure.snip1382:before {
-    top: 0;
-    -webkit-transition-delay: 0;
-    transition-delay: 0;
-}
-figure.snip1382:after {
-    top: 25%;
-    -webkit-transition-delay: 0.1s;
-    transition-delay: 0.1s;
-}
-figure.snip1382 figcaption:before {
-    top: 50%;
-    -webkit-transition-delay: 0.2s;
-    transition-delay: 0.2s;
-    z-index: -1;
-}
-figure.snip1382 figcaption:after {
-    top: 75%;
-    -webkit-transition-delay: 0.3s;
-    transition-delay: 0.3s;
-    z-index: -1;
-}
-figure.snip1382 figcaption {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    z-index: 2;
-    padding: 30px;
-}
-figure.snip1382 h2,
-figure.snip1382 p,
-figure.snip1382 .icons {
-    margin: 0;
-    width: 100%;
-    opacity: 0;
-}
-figure.snip1382 h2 {
-    font-weight: 900;
-    text-transform: uppercase;
-}
-figure.snip1382 p {
-    font-weight: 300;
-}
-figure.snip1382 .icons {
-    position: absolute;
-    bottom: 30px;
-    left: 0;
-    width: 100%;
-}
-figure.snip1382 i {
-    padding: 0px 10px;
-    display: inline-block;
-    font-size: 24px;
-    color: #ffffff;
-    text-align: center;
-    opacity: 0.8;
-    text-decoration: none;
-}
-figure.snip1382 i:hover {
-    opacity: 1;
-}
-figure.snip1382:hover:after,
-figure.snip1382.hover:after,
-figure.snip1382:hover:before,
-figure.snip1382.hover:before,
-figure.snip1382:hover figcaption:after,
-figure.snip1382.hover figcaption:after,
-figure.snip1382:hover figcaption:before,
-figure.snip1382.hover figcaption:before {
-    left: 0;
-    right: 0;
-    opacity: 0.8;
-}
-figure.snip1382:hover figcaption h2,
-figure.snip1382.hover figcaption h2,
-figure.snip1382:hover figcaption p,
-figure.snip1382.hover figcaption p,
-figure.snip1382:hover figcaption .icons,
-figure.snip1382.hover figcaption .icons {
-    -webkit-transition-delay: 0.25s;
-    transition-delay: 0.25s;
-}
-figure.snip1382:hover figcaption h2,
-figure.snip1382.hover figcaption h2,
-figure.snip1382:hover figcaption .icons,
-figure.snip1382.hover figcaption .icons {
-    opacity: 1;
-}
-figure.snip1382:hover figcaption p,
-figure.snip1382.hover figcaption p {
-    opacity: 0.7;
-}
 </style>
